4505d8af7ae10d82dfb70d5936bc07ff9ddb9338
yashkins <yashkins@yandex.ru>
2021-09-19 21:07:17 +1000

    добавлена игра в числа

--- bot.py
+++ bot.py
@@ -1,8 +1,10 @@
-import logging
 
-import settings 
-import ephem
 from datetime import date, datetime
+import ephem
+from glob import glob
+import logging
+from random import randint, choice
+import settings 
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, messagehandler
 
 
@@ -28,9 +30,30 @@ def definition_planet(update,context):
     constellation = ephem.constellation(planet)
     update.message.reply_text(constellation)
 
+
+def logic_play(user_digit):
+    bot_digit = randint(user_digit-10, user_digit+10)
+    result = "Вы выиграли" * (user_digit > bot_digit) + "Вы проиграли" * (user_digit < bot_digit)+ "Ничья" * (user_digit == bot_digit)
+    return f"Ваше число {user_digit} Мое число {bot_digit} \n" + result
+
+
+def play_digit(update, context):
+    if context.args:
+        try:
+            user_digit = int(context.args[0])
+            messages = logic_play(user_digit)
+        except (TypeError, ValueError):
+            messages = "Введите целое число"
+    else:
+        messages = "Введите число"
+    update.message.reply_text(messages)
+
+
+
 def main():
     mybot = Updater(settings.API_KEY, use_context = True, request_kwargs = PROXY)
     dp = mybot.dispatcher
+    dp.add_handler(CommandHandler("digit", play_digit))
     dp.add_handler(CommandHandler("start", greet_user))
     dp.add_handler(CommandHandler("planet", definition_planet))
     dp.add_handler(MessageHandler(Filters.text, talk_to_me))
